2904d5c11665635ea13aedd78665b738c49193af
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-02 17:48:34 +0100

    gestures

--- src/Components/BasicsOfMotion.jsx
+++ src/Components/BasicsOfMotion.jsx
@@ -9,17 +9,33 @@ const BasicsOfMotion = () => {
   return (
     <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
 
-    <button onClick={()=> setVisible(!visible)} className="example-button">Show-Hide</button>
+    <motion.button
+    layout
+     onClick={()=> setVisible(!visible)} className="example-button">
+    Show-Hide</motion.button>
      
 
-     <AnimatePresence>
+     <AnimatePresence mode='popLayout'>
             {visible && <motion.div 
-            initial ={{rotate: '0deg'}}
-            animate ={{rotate: '180deg'}}
-            exit ={{rotate: '0deg'}}
+            initial ={
+                {rotate: '0deg',
+                 scale: 0,
+                 y:0
+                }}
+            animate ={
+                {rotate: '180deg', 
+                scale: 1,
+                y:[0, 150, -150, -150, 0]
+            }}
+            exit ={
+                {rotate: '0deg', 
+                scale: 0,
+                y:0
+            }}
             transition={{
-                duration: 2,
-                ease: backInOut
+                duration: 1,
+                ease: backInOut,
+                times: [0, 0.25, 0.5, 0.85, 1 ]
             }}
             className="w-[150px] h-[150px] bg-black">
             </motion.div>}
