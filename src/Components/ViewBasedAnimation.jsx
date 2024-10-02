ffc436e36d8fa3fdafa1a0a627d1656fdc705411
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-03 00:10:22 +0100

    useInView

--- src/Components/ViewBasedAnimation.jsx
+++ src/Components/ViewBasedAnimation.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { motion, useInView } from 'framer-motion'
+import React, { useRef } from 'react'
 
 const ViewBasedAnimation = () => {
+
+    const ref = useRef(null)
+    const isInView = useInView(ref)
+
+
   return (
-    <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
-      
+    <div >
+
+        <div className=" h-[150vh] ">
+
+        </div>
+
+        <motion.div 
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:1}}
+        className='h-[100vh] bg-black'>
+
+        </motion.div>
+
+        <div
+        className='h-[100vh] bg-red-500'
+        transition = '1s background'
+        
+        ref={ref}
+        />
     </div>
   )
 }
