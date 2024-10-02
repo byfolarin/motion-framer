5ac0923b349eb4b2339f8a99a00421399dea7759
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-02 17:49:39 +0100

    mo-gesture

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import BasicsOfMotion from './Components/BasicsOfMotion'
+import Gestures from './Components/Gestures'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <BasicsOfMotion />
+      <Gestures />
     </>
   )
 }
