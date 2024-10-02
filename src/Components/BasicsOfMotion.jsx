a0c9ca8849a77d481834cd0334dae85cb7079938
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-02 17:29:24 +0100

    visible

--- src/Components/BasicsOfMotion.jsx
+++ src/Components/BasicsOfMotion.jsx
@@ -1,57 +1,30 @@
+import { easeInOut, motion } from "framer-motion";
+import { backInOut } from "framer-motion/dom";
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 
 const BasicsOfMotion = () => {
-  const [isVisible, setIsVisible] = useState(true);
+
+    const [visible,setVisible]= useState(true);
+ 
   return (
-    <div
-      style={{
-        display: "grid",
-        placeContent: "center",
-        height: "100vh",
-        gap: "0.8rem",
-      }}
-    >
-      <motion.button
-        onClick={() => setIsVisible(!isVisible)}
-        className="example-button"
-        layout
-      >
-        Show/Hide
-      </motion.button>
-      <AnimatePresence mode="popLayout">
-        {isVisible && (
-          <motion.div
-            initial={{
-              rotate: "0deg",
-              scale: 0,
-              y: 0,
-            }}
-            animate={{
-              rotate: "180deg",
-              scale: 1,
-              y: [0, 150, -150, -150, 0],
-            }}
-            exit={{
-              rotate: "0deg",
-              scale: 0,
-              y: 0,
-            }}
-            transition={{
-              duration: 1,
-              ease: "backInOut",
-              times: [0, 0.25, 0.5, 0.85, 1],
-            }}
-            style={{
-              width: 150,
-              height: 150,
-              background: "black",
-            }}
-          ></motion.div>
-        )}
-      </AnimatePresence>
+    <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
+
+        <button onClick={()=> setVisible(!visible)} className="example-button">Show-Hide</button>
+     
+     {visible && <motion.div 
+     initial ={{rotate: '0deg'}}
+     animate ={{rotate: '180deg'}}
+     exit ={{rotate: '0deg'}}
+     transition={{
+        duration: 2,
+        ease: backInOut
+     }}
+     className="w-[150px] h-[150px] bg-black">
+
+     </motion.div>}
+       
     </div>
-  );
-};
+  )
+}
 
 export default BasicsOfMotion;
