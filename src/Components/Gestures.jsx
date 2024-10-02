ff6135046773da7c269374eb8d3c456c85843b59
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-02 18:35:39 +0100

    gestures

--- src/Components/Gestures.jsx
+++ src/Components/Gestures.jsx
@@ -1,9 +1,75 @@
+import { motion, MotionConfig } from 'framer-motion'
+import { easeInOut } from 'framer-motion/dom'
 import React from 'react'
+
 
 const Gestures = () => {
   return (
     <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
-      
+
+
+      <MotionConfig
+        transition={{
+          duration: '0.125',
+          ease: easeInOut
+        }}
+      >
+
+          <motion.button 
+          whileHover={{
+            scale: 1.05,
+          }}
+          whileTap={{
+            scale: 0.95, rotate: '2.5deg'
+          }}
+          
+          className='example-button' >Click Me!
+          </motion.button>
+
+
+          <motion.button 
+          whileHover={{
+            scale: 1.05,
+          }}
+          whileTap={{
+            scale: 0.95, rotate: '2.5deg'
+          }}
+          className='examples-button ' >Click Me!
+          </motion.button>
+
+      </MotionConfig>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
