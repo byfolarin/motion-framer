2b20c90d79f4a729da153db61a50a13b3b673e72
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-02 18:47:14 +0100

    selection

--- src/Components/AnimationControls.jsx
+++ src/Components/AnimationControls.jsx
@@ -1,9 +1,18 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 const AnimationControls = () => {
   return (
-    <div>
-      
+    <div className="grid place-content-center h-[100vh] gap-[0.8rem]">
+     
+
+
+        <button className='example-button'>Flip it!</button>
+        
+        <motion.div className='w-[150px] h-[150px] bg-black'>
+            
+        </motion.div>
+
     </div>
   )
 }
