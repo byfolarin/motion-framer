9b8ea8a16705855a6defe504463e0de0eb63edb6
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-04 17:06:20 +0100

    ikd

--- src/Components/TextBased.jsx
+++ src/Components/TextBased.jsx
@@ -13,17 +13,20 @@ const [isVisible, setIsVisible] = useState(true)
         className='example-button'>Let's Go!
         </motion.button>
 
-    {isVisible && <motion.div
+    {isVisible && <motion.div 
+        
        initial ={{scale:0, 
         y:0,
+        
         rotate:'0deg'}}
 
        animate ={{scale:1,
          y:[0,-600,600,0],
-        rotate:['10deg', '0deg']}}
+         borderRadius:[90,10, 90,0],
+        rotate:[0, 90, 20, 0]}}
 
        exit={{scale:0,
-         rotate:'0deg'}}
+         rotate:0}}
 
        transition={{ease:easeInOut, duration: 1}} 
       className='w-64 h-64 bg-black'></motion.div>}
