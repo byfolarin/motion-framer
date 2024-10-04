088dcbbc9686488e0e2bb1dc16fb6fa129165ac8
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-04 16:17:13 +0100

    button

--- src/Components/TextBased.jsx
+++ src/Components/TextBased.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const TextBased = () => {
   return (
-    <div>
-      <motion.div></motion.div>
+    <div className='grid place-content-center h-[100vh] gap-[0.8rem]'>
+      <motion.div className='w-64 h-64 bg-black'></motion.div>
 
       <motion.div></motion.div>
     </div>
