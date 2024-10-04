88fc4855746c1579c88ac8547604051a2db19edc
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-04 17:11:00 +0100

    levered

--- src/Components/GesturesLearn.jsx
+++ src/Components/GesturesLearn.jsx
@@ -1,9 +1,20 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+
 
 const GesturesLearn = () => {
   return (
     <div>
-      
+        <motion.button 
+          whileHover={{
+            scale: 1.05,
+          }}
+          whileTap={{
+            scale: 0.95, rotate: '2.5deg'
+          }}
+          
+          className='example-button' >Click Me!
+          </motion.button>
     </div>
   )
 }
