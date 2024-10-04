735cdd9ba4ec856743e50185f1ddbe8e19fc13ed
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-04 16:49:46 +0100

    visible

--- src/Components/TextBased.jsx
+++ src/Components/TextBased.jsx
@@ -1,19 +1,34 @@
-import { easeInOut, motion } from 'framer-motion'
-import React from 'react'
+import { easeInOut, motion, useScroll } from 'framer-motion'
+import React, { useState } from 'react'
+
 
 const TextBased = () => {
+
+    const {scrollYProgress} = useScroll()
+
+    const [isVisible, setIsVisible] = useState(true)
+
   return (
     <div className='grid place-content-center h-[100vh] gap-[0.8rem]'>
-        <motion.button 
+        <motion.button
+        onClick={()=>setIsVisible(!isVisible)} 
         className='example-button'>Let's Go!
         </motion.button>
 
-      <motion.div
-       initial ={{scale:0, y:0, rotate:'0deg'}}
-       animate ={{scale:1, y:[0,-600,600,0], rotate:['10deg', '0deg']}}
-       exit={{scale:0, rotate:'0deg'}}
+    {isVisible && <motion.div
+       initial ={{scale:0, 
+        y:0,
+        rotate:'0deg'}}
+
+       animate ={{scale:1,
+         y:[0,-600,600,0],
+        rotate:['10deg', '0deg']}}
+
+       exit={{scale:0,
+         rotate:'0deg'}}
+
        transition={{ease:easeInOut, duration: 1}} 
-      className='w-64 h-64 bg-black'></motion.div>
+      className='w-64 h-64 bg-black'></motion.div>}
     </div>
   )
 }
