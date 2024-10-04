014781d13d03fd785ee9f02ffa7860ffd1664e0f
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-04 16:39:07 +0100

    settings

--- src/Components/TextBased.jsx
+++ src/Components/TextBased.jsx
@@ -1,12 +1,19 @@
-import { motion } from 'framer-motion'
+import { easeInOut, motion } from 'framer-motion'
 import React from 'react'
 
 const TextBased = () => {
   return (
     <div className='grid place-content-center h-[100vh] gap-[0.8rem]'>
-      <motion.div className='w-64 h-64 bg-black'></motion.div>
+        <motion.button 
+        className='example-button'>Let's Go!
+        </motion.button>
 
-      <motion.div></motion.div>
+      <motion.div
+       initial ={{scale:0, y:0, rotate:'0deg'}}
+       animate ={{scale:1, y:[0,-600,600,0], rotate:['10deg', '0deg']}}
+       exit={{scale:0, rotate:'0deg'}}
+       transition={{ease:easeInOut, duration: 1}} 
+      className='w-64 h-64 bg-black'></motion.div>
     </div>
   )
 }
